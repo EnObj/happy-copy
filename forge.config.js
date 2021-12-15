e4c63ef55c04d066bfa90e9de9d90538916a8dbf
jienhui <enhui_ji@qq.com>
2021-12-15 17:35:06 +0800

    图标更新

--- forge.config.js
+++ forge.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "makers": [{
             "name": "@electron-forge/maker-squirrel",
             "config": {
+                "iconUrl": "https://6465-dev-9g0suwuw61afb9f3-1252108641.tcb.qcloud.la/enobj/favicon.ico",
                 "setupIcon": path.join(__dirname, '/static/image/favicon.ico')
             }
         },
